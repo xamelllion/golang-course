b571b30450deed5aafd0770703e27838ed9f5a90
Viacheslav Sidorov <me@xamelllion.ru>
2026-06-18 07:47:08 +0300

    Feat: add postgresql service and first migrations

--- subscriber/internal/config/config.go
+++ subscriber/internal/config/config.go
@@ -3,13 +3,15 @@ package config
 import "os"
 
 type Config struct {
-	Port string
+	Port   string
+	DB_DSN string
 }
 
 func Load() Config {
 	var config Config
 
 	config.Port = getenv("SUBSCRIBER_PORT", "50053")
+	config.DB_DSN = getenv("DB_DSN", "postgres://postgres:postgres@localhost:5432/subscribers?sslmode=disable")
 
 	return config
 }
